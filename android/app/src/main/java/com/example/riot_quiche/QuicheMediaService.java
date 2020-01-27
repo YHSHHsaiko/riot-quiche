2f9ab9921dc308278520d69df128b983d67cf15d
YHSHHsaiko <kithzun29765@outlook.jp>
2020-01-28 02:20:14 +0900

    impelementation(beta): quicheForeGroundService

--- android/app/src/main/java/com/example/riot_quiche/QuicheMediaService.java
+++ android/app/src/main/java/com/example/riot_quiche/QuicheMediaService.java
@@ -10,6 +10,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.provider.MediaStore;
 import android.support.v4.media.MediaBrowserCompat;
+import android.support.v4.media.MediaDescriptionCompat;
 import android.support.v4.media.MediaMetadataCompat;
 import android.support.v4.media.session.MediaControllerCompat;
 import android.support.v4.media.session.MediaSessionCompat;
@@ -17,8 +18,10 @@ import android.support.v4.media.session.PlaybackStateCompat;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
 import androidx.media.AudioManagerCompat;
 import androidx.media.MediaBrowserServiceCompat;
+import androidx.media.session.MediaButtonReceiver;
 
 import com.google.android.exoplayer2.ExoPlayer;
 import com.google.android.exoplayer2.ExoPlayerFactory;
@@ -39,6 +42,9 @@ public class QuicheMediaService extends MediaBrowserServiceCompat {
     private static final String MEDIA_ROOT_ID = "quiche";
     private static final String MEDIA_EMPTY_ROOT_ID = "nirvana";
     private static final String LOG_TAG = "quiche_log";
+    private static final String NOTIFICATION_CHANNEL = "riot-quiche";
+
+    private int notificationVisibility = NotificationCompat.VISIBILITY_PUBLIC;
     private MediaSessionCompat mediaSession;
     private PlaybackStateCompat.Builder stateBuilder;
     private QuicheLibrary library;
@@ -46,6 +52,48 @@ public class QuicheMediaService extends MediaBrowserServiceCompat {
     private AudioManager audioManager;
     private Handler handler;
     private MediaSessionCompat.Callback mediaSessionCallback = new MediaSessionCompat.Callback() {
+        private AudioManager.OnAudioFocusChangeListener onAudioFocusChangeListener =
+            new AudioManager.OnAudioFocusChangeListener() {
+                @Override
+                public void onAudioFocusChange(int focusChange) {
+                    switch (focusChange) {
+                        // on focus lost
+                        case AudioManager.AUDIOFOCUS_LOSS: {
+                            mediaSession.getController().getTransportControls().pause();
+                            break;
+                        }
+                        // on focus lost temporary
+                        case AudioManager.AUDIOFOCUS_GAIN_TRANSIENT: {
+                            mediaSession.getController().getTransportControls().pause();
+                            break;
+                        }
+                        // on ducking
+                        case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK: {
+                            /* TODO: volume 下げる　おそらくducking終わったら戻さないと
+                            *   audioManager.setVolume(volume_level, AudioManager.STREAM_MUSIC)*/
+                            break;
+                        }
+                        case AudioManager.AUDIOFOCUS_GAIN: {
+                            mediaSession.getController().getTransportControls().play();
+                            break;
+                        }
+                        default: {
+                            break;
+                        }
+                    }
+                }
+        };
+        private AudioAttributes audioAttributes = new AudioAttributes.Builder()
+                .setUsage(AudioAttributes.USAGE_MEDIA)
+                .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                .build();
+        private AudioFocusRequest audioFocusRequest =
+            new AudioFocusRequest.Builder(AudioManager.AUDIOFOCUS_GAIN)
+                .setAudioAttributes(audioAttributes)
+                .setAcceptsDelayedFocusGain(true)
+                .setWillPauseWhenDucked(true)
+                .setOnAudioFocusChangeListener(onAudioFocusChangeListener)
+                .build();
 
         @Override
         public void onPlayFromMediaId (String mediaId, Bundle extras) {
@@ -72,25 +120,84 @@ public class QuicheMediaService extends MediaBrowserServiceCompat {
 
         @Override
         public void onPlay () {
-            AudioAttributes audioAttributes = new AudioAttributes.Builder()
-                    .setUsage(AudioAttributes.USAGE_MEDIA)
-                    .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
-                    .build();
-            AudioFocusRequest afr = new AudioFocusRequest.Builder(AudioManager.AUDIOFOCUS_GAIN)
-                    .setAudioAttributes(audioAttributes)
-                    .setAcceptsDelayedFocusGain(true)
-                    .setWillPauseWhenDucked(true)
-                    .setOnAudioFocusChangeListener((int focusChange) -> {
-                        // TODO: on focus change ここで何するねん・・・・
-                    })
-                    .build();
-            if (audioManager.requestAudioFocus(afr) == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+            if (audioManager.requestAudioFocus(audioFocusRequest) == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
                 // activate media session
                 mediaSession.setActive(true);
                 exoPlayer.setPlayWhenReady(true);
             }
         }
+
+        @Override
+        public void onPause () {
+            exoPlayer.setPlayWhenReady(false);
+            audioManager.abandonAudioFocusRequest(audioFocusRequest);
+        }
+
+        @Override
+        public void onSeekTo (long position) {
+            exoPlayer.seekTo(position);
+        }
+
+        @Override
+        public void onSkipToNext () {
+            // TODO: 次の曲をリクエスト．．．？次の曲って？
+        }
+
+        @Override
+        public void onSkipToPrevious () {
+            // TODO: 前の曲をリクエスト・・・？前の曲って？
+        }
+
+        @Override
+        public void onSkipToQueueItem (long i) {
+            // TODO: キューの存在をまず調べなければならない．．．
+        }
     };
+    private Player.EventListener exoPlayerEventListener = new Player.EventListener() {
+
+        @Override
+        public void onPlayerStateChanged (boolean playWhenReady, int playbackState) {
+            updatePlaybackState(playWhenReady, playbackState);
+        }
+    };
+
+    private void updatePlaybackState (boolean playWhenReady, int playbackState) {
+        int state = PlaybackStateCompat.STATE_NONE;
+
+        switch (playbackState) {
+            case Player.STATE_IDLE: {
+                state = PlaybackStateCompat.STATE_NONE;
+                break;
+            }
+            case Player.STATE_BUFFERING: {
+                state = PlaybackStateCompat.STATE_BUFFERING;
+                break;
+            }
+            case Player.STATE_READY: {
+                if (playWhenReady) {
+                    state = PlaybackStateCompat.STATE_PLAYING;
+                } else {
+                    state = PlaybackStateCompat.STATE_PAUSED;
+                }
+                break;
+            }
+            case Player.STATE_ENDED: {
+                state = PlaybackStateCompat.STATE_STOPPED;
+                break;
+            }
+            default: {
+                state = PlaybackStateCompat.STATE_NONE;
+                break;
+            }
+        }
+
+        mediaSession.setPlaybackState(new PlaybackStateCompat.Builder()
+                .setActions(PlaybackStateCompat.ACTION_PLAY | PlaybackStateCompat.ACTION_PAUSE |
+                        PlaybackStateCompat.ACTION_SKIP_TO_NEXT | PlaybackStateCompat.ACTION_STOP)
+                .setState(state, exoPlayer.getCurrentPosition(), exoPlayer.getPlaybackParameters().speed)
+                .build()
+        );
+    }
 
     @Override
     public void onCreate () {
@@ -111,10 +218,8 @@ public class QuicheMediaService extends MediaBrowserServiceCompat {
         );
 
         stateBuilder = new PlaybackStateCompat.Builder()
-                .setActions(
-                        PlaybackStateCompat.ACTION_PLAY |
-                        PlaybackStateCompat.ACTION_PAUSE |
-                        PlaybackStateCompat.ACTION_PLAY_PAUSE);
+                .setActions(PlaybackStateCompat.ACTION_PLAY | PlaybackStateCompat.ACTION_PAUSE |
+                        PlaybackStateCompat.ACTION_SKIP_TO_NEXT | PlaybackStateCompat.ACTION_STOP);
 
         mediaSession.setPlaybackState(stateBuilder.build());
         mediaSession.setCallback(mediaSessionCallback);
@@ -143,13 +248,80 @@ public class QuicheMediaService extends MediaBrowserServiceCompat {
         handler.postDelayed(new Runnable () {
            @Override
            public void run () {
-               if (exoPlayer.getPlaybackState() == Player.STATE_READY && exoPlayer.getPlayWhenReady()) {
-                   updatePlaybackState();
+               int state = exoPlayer.getPlaybackState();
+               if (state == Player.STATE_READY && exoPlayer.getPlayWhenReady()) {
+                   updatePlaybackState(true, state);
                }
 
                handler.postDelayed(this, 500);
            }
         }, 500);
+    }
+
+    private void notificate () {
+        MediaControllerCompat mediaController = mediaSession.getController();
+        MediaMetadataCompat metadata = mediaController.getMetadata();
+
+        if (metadata == null && !mediaSession.isActive()) {
+            return;
+        }
+
+        MediaDescriptionCompat description = metadata.getDescription();
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(
+                getApplicationContext(), NOTIFICATION_CHANNEL);
+
+        // TODO: intent作る　いらない気もする．．．タップして開きたいなら必要
+        builder
+                .setContentTitle(description.getTitle())
+                .setSubText(description.getDescription())
+                .setLargeIcon(description.getIconBitmap())
+
+//                .setContentIntent(notificationIntent)
+
+                .setDeleteIntent(MediaButtonReceiver.buildMediaButtonPendingIntent(
+                        this, PlaybackStateCompat.ACTION_STOP))
+
+                .setVisibility(notificationVisibility)
+
+                .setSmallIcon(R.drawable.exo_controls_play)
+//                .setColor()
+//                .setStyle()
+        ;
+
+        // add actions
+        builder.addAction(new NotificationCompat.Action(
+                R.drawable.exo_controls_previous, "prev",
+                MediaButtonReceiver.buildMediaButtonPendingIntent(
+                        this,
+                        PlaybackStateCompat.ACTION_SKIP_TO_PREVIOUS
+                )
+        ));
+        builder.addAction(new NotificationCompat.Action(
+                R.drawable.exo_controls_next, "next",
+                MediaButtonReceiver.buildMediaButtonPendingIntent(this,
+                        PlaybackStateCompat.ACTION_SKIP_TO_NEXT))
+        );
+        if (mediaController.getPlaybackState().getState() == PlaybackStateCompat.STATE_PLAYING) {
+            builder.addAction(new NotificationCompat.Action(
+                    R.drawable.exo_controls_pause, "pause",
+                    MediaButtonReceiver.buildMediaButtonPendingIntent(this,
+                            PlaybackStateCompat.ACTION_PAUSE)));
+        } else {
+            builder.addAction(new NotificationCompat.Action(
+                    R.drawable.exo_controls_play, "play",
+                    MediaButtonReceiver.buildMediaButtonPendingIntent(this,
+                            PlaybackStateCompat.ACTION_PLAY)));
+        }
+
+        if (mediaController.getPlaybackState().getState() == PlaybackStateCompat.STATE_PLAYING) {
+            // foreground notification (cannot delete)
+            startForeground(1, builder.build());
+        } else {
+            // when not playing, background notification (can delete)
+            stopForeground(false);
+        }
+
     }
 
     @Nullable
